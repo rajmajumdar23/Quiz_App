88e7958cbead94c6b34f439b105439ef3ee0f8c7
rajmajumdar23 <141353400+rajmajumdar23@users.noreply.github.com>
2023-08-08 00:49:59 +0530

    Update QuizResult.jsx

--- src/Component/QuizResult.jsx
+++ src/Component/QuizResult.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Styling/Quiz.css";
 
-const QuizResult = ({ score, name, quizData }) => {
+const QuizResult = ({ score, name,}) => {
 
   return (
     <div className="result-section">
       <p>Hello, {name}! Here's your quiz result:</p>
-      <p>You scored: {score} out of {quizData.length}</p>
+      <p>You scored: {score} out of 12</p>
     </div>
   );
 };
