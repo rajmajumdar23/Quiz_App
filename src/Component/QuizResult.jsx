665d925b41f5054695c8b300415f60f7671d8fd6
rajmajumdar23 <141353400+rajmajumdar23@users.noreply.github.com>
2023-08-08 00:59:44 +0530

    Update QuizResult.jsx

--- src/Component/QuizResult.jsx
+++ src/Component/QuizResult.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import the Link component from React Router
 import "./Styling/Quiz.css";
 
-const QuizResult = ({ score, name,}) => {
-
+const QuizResult = ({ score, name }) => {
   return (
     <div className="result-section">
       <p>Hello, {name}! Here's your quiz result:</p>
       <p>You scored: {score} out of 12</p>
+      <Link to="/">
+        <button>Play Again</button>
+      </Link>
     </div>
   );
 };
