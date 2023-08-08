a71a96d52cf674fa796a65a89af2c6fdc92d3058
rajmajumdar23 <141353400+rajmajumdar23@users.noreply.github.com>
2023-08-08 09:53:07 +0530

    Update LevelSelection.jsx

--- src/Component/LevelSelection.jsx
+++ src/Component/LevelSelection.jsx
@@ -1,18 +1,18 @@
-// LevelSelection.js
-import React from "react";
-import "./Styling/LevelSelection.css";
+  // LevelSelection.js
+  import React from "react";
+  import "./Styling/LevelSelection.css";
 
-const LevelSelection = ({ handleLevelSelect, name }) => {
-  return (
-    <div className="level-section">
-      <p id="Text">Hello, {name}! Select a level:</p>
-      <div className="button-container">
-        <button onClick={() => handleLevelSelect("easy")}>Easy</button>
-        <button onClick={() => handleLevelSelect("medium")}>Medium</button>
-        <button onClick={() => handleLevelSelect("hard")}>Hard</button>
+  const LevelSelection = ({ handleLevelSelect, name }) => {
+    return (
+      <div className="level-section">
+        <p id="Text">Hello, {name}! Select a level:</p>
+        <div className="button-container">
+          <button onClick={() => handleLevelSelect("easy")}>Easy</button>
+          <button onClick={() => handleLevelSelect("medium")}>Medium</button>
+          <button onClick={() => handleLevelSelect("hard")}>Hard</button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default LevelSelection;
+  export default LevelSelection;
